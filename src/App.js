cdbace1022a33159f8259d9616cf5c06949c3f85
Divya Nair <divnairs@gmail.com>
2019-03-19 15:30:16 -0700

    render styled number buttons

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import React, { Component } from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-       hi
-      </div>
-    );
-  }
+export default function App() {
+  const numSymBtn = [
+    [7, 'number'],
+    [8, 'number'],
+    [9, 'number'],
+    [<i class="fas fa-divide" />, 'symbol'],
+    [4, 'number'],
+    [5, 'number'],
+    [6, 'number'],
+    [<i class="fas fa-times" />, 'symbol'],
+    [1, 'number'],
+    [2, 'number'],
+    [3, 'number'],
+    [<i class="fas fa-minus" />, 'symbol'],
+    [0, 'number'],
+    ['.', 'symbol'],
+    [<i class="fas fa-equals" />, 'symbol'],
+    [<i class="fas fa-plus" />, 'symbol']
+  ]
+
+  return (
+    <div className="App">
+      {numSymBtn.map(btn => (
+        <button className={btn[1]}>{btn[0]}</button>
+      ))}
+    </div>
+  )
 }
-
-export default App;
