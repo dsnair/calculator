3cca8ff1b5745210ce916d98f5e3727be8afd37a
Divya Nair <divnairs@gmail.com>
2019-03-19 16:11:52 -0700

    add onClick to capture number button clicks

--- src/App.js
+++ src/App.js
@@ -1,30 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
 export default function App() {
+  const [numSym, setNumSym] = useState('')
+  const handleNumSymClick = (val) => setNumSym(console.log(val))
+
   const numSymBtn = [
     [7, 'number'],
     [8, 'number'],
     [9, 'number'],
-    [<i class="fas fa-divide" />, 'symbol'],
+    [<i className="fas fa-divide" />, 'symbol'],
     [4, 'number'],
     [5, 'number'],
     [6, 'number'],
-    [<i class="fas fa-times" />, 'symbol'],
+    [<i className="fas fa-times" />, 'symbol'],
     [1, 'number'],
     [2, 'number'],
     [3, 'number'],
-    [<i class="fas fa-minus" />, 'symbol'],
+    [<i className="fas fa-minus" />, 'symbol'],
     [0, 'number'],
     ['.', 'symbol'],
-    [<i class="fas fa-equals" />, 'symbol'],
-    [<i class="fas fa-plus" />, 'symbol']
+    [<i className="fas fa-equals" />, 'symbol'],
+    [<i className="fas fa-plus" />, 'symbol']
   ]
 
   return (
     <div className="App">
-      {numSymBtn.map(btn => (
-        <button className={btn[1]}>{btn[0]}</button>
+      {numSymBtn.map((btn, index) => (
+        <button
+          onClick={() => handleNumSymClick(btn[3])}
+          className={btn[1]}
+          key={index}
+        >
+          {btn[0]}
+        </button>
       ))}
       <button className="clear">clear</button>
     </div>
