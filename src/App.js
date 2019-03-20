5e902ebf88a389f10dee52ae6338b803c8426b20
Divya Nair <divnairs@gmail.com>
2019-03-19 20:51:14 -0700

    register button clicks to form an expression

--- src/App.js
+++ src/App.js
@@ -3,32 +3,32 @@ import './App.css'
 
 export default function App() {
   const [numSym, setNumSym] = useState('')
-  const handleNumSymClick = (val) => setNumSym(console.log(val))
+  const handleNumSymClick = exp => setNumSym(numSym.concat(exp))
 
   const numSymBtn = [
-    [7, 'number'],
-    [8, 'number'],
-    [9, 'number'],
-    [<i className="fas fa-divide" />, 'symbol'],
-    [4, 'number'],
-    [5, 'number'],
-    [6, 'number'],
-    [<i className="fas fa-times" />, 'symbol'],
-    [1, 'number'],
-    [2, 'number'],
-    [3, 'number'],
-    [<i className="fas fa-minus" />, 'symbol'],
-    [0, 'number'],
-    ['.', 'symbol'],
-    [<i className="fas fa-equals" />, 'symbol'],
-    [<i className="fas fa-plus" />, 'symbol']
+    [7, 'number', '7'],
+    [8, 'number', '8'],
+    [9, 'number', '9'],
+    [<i className="fas fa-divide" />, 'symbol', '/'],
+    [4, 'number', '4'],
+    [5, 'number', '5'],
+    [6, 'number', '6'],
+    [<i className="fas fa-times" />, 'symbol', '*'],
+    [1, 'number', '1'],
+    [2, 'number', '2'],
+    [3, 'number', '3'],
+    [<i className="fas fa-minus" />, 'symbol', '-'],
+    [0, 'number', '0'],
+    ['.', 'symbol', '.'],
+    [<i className="fas fa-equals" />, 'symbol', '='],
+    [<i className="fas fa-plus" />, 'symbol', '+']
   ]
 
   return (
     <div className="App">
       {numSymBtn.map((btn, index) => (
         <button
-          onClick={() => handleNumSymClick(btn[3])}
+          onClick={() => handleNumSymClick(btn[2])}
           className={btn[1]}
           key={index}
         >
